2def0d6d17303752ab4403c65f9a1ec97ba1702f
RonyaShaa <ronya.shaa96@yandex.ru>
2023-05-04 14:37:50 +0300

    add closeByEscope/handleOverlay

--- src/components/ImagePopup.jsx
+++ src/components/ImagePopup.jsx
@@ -2,9 +2,31 @@ import React from 'react';
 
 function ImagePopup({onClose, card, isOpen}) {
 
+  React.useEffect(() => {
+    //ограничиваем навешивание обработчика: если не открыт, то не нужно навешивать
+    if(!isOpen) return;
+    // объявляем внутри useEffect функцию, чтобы она не теряла ссылку при перерисовке компонента
+    const closeByEsc = (e) => {
+      if(e.key === 'Escape') {
+        onClose();
+      }
+    }
+    
+    document.addEventListener('keydown', closeByEsc)
+    //обязательно удаляем обработчик
+    return () => document.removeEventListener('keydown', closeByEsc)
+    // обязательно следим за isOpen, чтобы срабатывало только при открытии, а не всегда
+  }, [isOpen, onClose])
+
+  //создаем обработчик оверлея
+  const handleOverlay = (e) => {
+    if(e.target === e.currentTarget) {
+      onClose();
+    }
+  }
 
   return (
-      <div className={`popup popup_type_expand-card ${isOpen ? 'popup_opened' : ''}`}>
+      <div className={`popup popup_type_expand-card ${isOpen ? 'popup_opened' : ''}`} onClick={handleOverlay}>
         <div className="popup__container-fullscreen">
           <button className="popup__close popup__close_expand_card" type="button" onClick={onClose}></button>
           <img src={card.link} alt={card.name} className="popup__photo" />
--- src/components/PopupWithForm.jsx
+++ src/components/PopupWithForm.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function PopupWithForm({title, name ,buttonText, children, isOpen , onClose, onSubmit}) {
+  React.useEffect(() => {
+    //ограничиваем навешивание обработчика: если не открыт, то не нужно навешивать
+    if(!isOpen) return;
+    // объявляем внутри useEffect функцию, чтобы она не теряла ссылку при перерисовке компонента
+    const closeByEsc = (e) => {
+      if(e.key === 'Escape') {
+        onClose();
+      }
+    }
+    
+    document.addEventListener('keydown', closeByEsc)
+    //обязательно удаляем обработчик
+    return () => document.removeEventListener('keydown', closeByEsc)
+    // обязательно следим за isOpen, чтобы срабатывало только при открытии, а не всегда
+  }, [isOpen, onClose])
+
+  //создаем обработчик оверлея
+  const handleOverlay = (e) => {
+    if(e.target === e.currentTarget) {
+      onClose();
+    }
+  }
+  
   return (
-      <div className={`popup popup_type_${name} ${isOpen ? 'popup_opened' : ''}`}>  
+      <div className={`popup popup_type_${name} ${isOpen ? 'popup_opened' : ''}`} onClick={handleOverlay}>  
         <div className="popup__container">
           <button className="popup__close" type="button" onClick={onClose}/>
           <h2 className="popup__name">{title}</h2>
